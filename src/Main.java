8c13d8c7eee0936055b413db7c972212448d3f90
CzTamás <128179633+CzuprikTamas@users.noreply.github.com>
2023-08-31 18:33:24 +0200

    minimum method

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
+        int[] array1 = {23,5,4,56,567,8,3};
+        System.out.println(minimum(array1));
+
         int[] array = new int[20];
         System.out.println(Arrays.toString(method(array)));
     }
@@ -16,5 +19,15 @@ public class Main {
             array[i] = rndNum;
         }
         return array;
+    }
+
+    public static int minimum(int[] array) {
+        int minNum = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < minNum) {
+                minNum = array[i];
+            }
+        }
+       return minNum;
     }
 }
