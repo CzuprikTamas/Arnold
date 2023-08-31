1a48757104727fd487b80e39100c498947b225a1
CzTamás <128179633+CzuprikTamas@users.noreply.github.com>
2023-08-31 18:24:11 +0200

    create method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
+        int[] array = new int[20];
+        System.out.println(Arrays.toString(method(array)));
     }
 
+    public static int[] method(int[] array) {
+        Random random = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            int rndNum = random.nextInt(100) + 1;
+            array[i] = rndNum;
+        }
+        return array;
+    }
 }
